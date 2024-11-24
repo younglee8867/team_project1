d5bdc77700f7ce2e9ca8b1fb5b0f757d8cdae49f
rlamnji <rlamnji@naver.com>
2024-11-24 21:44:21 +0900

    edit

--- lib/SearchSta/test.dart
+++ lib/SearchSta/test.dart
@@ -282,3 +282,45 @@
     );
   }
 }*/
+
+
+/*import 'package:flutter/material.dart';
+import 'package:easy_localization/easy_localization.dart';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_application_1/SearchSta/SearchSta.dart';
+import 'package:flutter_application_1/SearchSta/SearchStaInfo.dart';
+
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(
+    EasyLocalization(
+      supportedLocales: const [Locale('en'), Locale('ko')],
+      path: 'assets/langs', // JSON 파일 경로
+      fallbackLocale: const Locale('ko'), // 기본 언어를 한국어로 설정
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
+      home: SearchStationPage(),
+    );
+  }
+}*/
