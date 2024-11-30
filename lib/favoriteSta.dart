b6d0857494d0adefe4d9408ccc503f58c7077484
rlamnji <rlamnji@naver.com>
2024-12-01 00:20:09 +0900

    즐찾 '역' 추가

--- lib/favoriteSta.dart
+++ lib/favoriteSta.dart
@@ -73,7 +73,7 @@ class _FavoriteStaState extends State<FavoriteSta> {
               itemBuilder: (context, index) {
                 final record = favoriteOnly[index];
                 return SearchResultItem(
-                  stationName: record['name'],
+                  stationName: record['name'] +"역".tr(),
                   favImagePath: record['isFavorite']
                       ? 'assets/images/favStarFill.png'
                       : 'assets/images/favStar.png',
